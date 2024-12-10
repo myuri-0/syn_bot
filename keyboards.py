4e9d8fcffe3ee8c9a7e529f4857451bdae7ca1d4
deadshot <anton2004lp@yzndex.ru>
2024-12-10 11:56:10 +0300

    Initial commit

--- keyboards.py
+++ keyboards.py
@@ -1,6 +1,4 @@
-from aiogram.types import ReplyKeyboardRemove, \
-    ReplyKeyboardMarkup, KeyboardButton, \
-    InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton
 
 
 @dp.message_handler(commands=['start'])
