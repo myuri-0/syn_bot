f7cdfd6ae5c18642eeb35419690e6c911fd738bd
ilya <ilya.05@mail.ru>
2024-12-10 12:10:36 +0300

    кнопки

--- message_router.py
+++ message_router.py
@@ -1,7 +1,8 @@
 from aiogram import Router, types
 from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram.types import Message, ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton
 import keyboards as kb
+
 
 router = Router()
 
@@ -10,7 +11,18 @@ router = Router()
 async def cmd_start(message: Message):
     await message.answer(f"Hello world")
 
+@router.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    kb = [
+        [
+            types.KeyboardButton(text="Узнать погоду"),
+            types.KeyboardButton(text="А хта это такэ?")
+        ],
+    ]
+    keyboard = types.ReplyKeyboardMarkup(keyboard=kb)
 
+    await message.reply("Привет!\nЯ Эхобот от Skillbox!\nОтправь мне любое сообщение, а я тебе обязательно отвечу.",
+                        reply_markup=keyboard)
 
 @router.message(Command("тест"))
 async def cmd_start(message: Message):
