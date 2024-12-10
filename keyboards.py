48a36ae6d962b4fd3c391181b4400c9796740dfd
deadshot <anton2004lp@yzndex.ru>
2024-12-10 11:54:41 +0300

    Initial commit

--- keyboards.py
+++ keyboards.py
@@ -3,3 +3,15 @@ from aiogram.types import ReplyKeyboardRemove, \
     InlineKeyboardMarkup, InlineKeyboardButton
 
 
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    kb = [
+        [
+            types.KeyboardButton(text="Узнать погоду"),
+            types.KeyboardButton(text="А хта это такэ?")
+        ],
+    ]
+    keyboard = types.ReplyKeyboardMarkup(keyboard=kb)
+
+    await message.reply("Привет!\nЯ Эхобот от Skillbox!\nОтправь мне любое сообщение, а я тебе обязательно отвечу.",
+                        reply_markup=keyboard)
