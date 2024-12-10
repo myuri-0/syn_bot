6c027e20daf25ab43fb1bc1a3328974fa2cd1ea8
ilya <ilya.05@mail.ru>
2024-12-10 09:28:10 +0300

    Initial commit

--- main.py
+++ main.py
@@ -1,23 +1,18 @@
-from aiogram import Bot, types
-from aiogram.dispatcher import Dispatcher
+import os
+from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
-from config import my_token
-TOKEN = my_token
-bot = Bot(token=TOKEN)
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_TOKEN = os.getenv('BOT_TOKEN')
+
+bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
-
 @dp.message_handler(commands=['start', 'help'])
-async def send_welcome(msg: types.Message):
-    await msg.reply_to_message(f'Я бот. Приятно познакомиться, {msg.from_user.first_name}')
-
-
-@dp.message_handler(content_types=['text'])
-async def get_text_messages(msg: types.Message):
-   if msg.text.lower() == 'привет':
-       await msg.answer('Привет!')
-   else:
-       await msg.answer('Не понимаю, что это значит.')
+async def send_welcome(message: types.Message):
+    await message.reply("Привет! Я бот, созданный с использованием aiogram.")
 
 if __name__ == '__main__':
-   executor.start_polling(dp)
+    executor.start_polling(dp, skip_updates=True)
